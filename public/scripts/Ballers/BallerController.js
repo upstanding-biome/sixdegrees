5af91c8284f1433d2196fe0655ceca0b737ca1c4
ChristopherSalam <christopher.salam@gmail.com>
2015-08-11 17:57:08 -0700

    Reduced controllers and simplified logic for search highlighting.

---
+++ public/scripts/Ballers/BallerController.js
@@ -0,0 +1,9 @@
+ app.controller('BallerController', function($scope){
+  //Search and SearchText are separate functions in angular
+  //We elected not to write a custom function and use these two over different players.
+  $scope.searchText = {};
+  $scope.searchText.name = '';
+  //This is used for the second basketball player.
+  $scope.search = {};
+  $scope.search.name = '';
+ });
