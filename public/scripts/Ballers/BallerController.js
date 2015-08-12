970da5c46cee4f44b90973b58ce12e868f939aeb
ChristopherSalam <christopher.salam@gmail.com>
2015-08-12 16:41:24 -0700

    Query string valid!

--- public/scripts/Ballers/BallerController.js
+++ public/scripts/Ballers/BallerController.js
@@ -8,59 +8,27 @@ app.controller('BallerController', function($scope, $http){
   $scope.search.name = '';
 
   $scope.callDB = function(name1, name2) {
-    $http.post('http://localhost:7474/db/data/cypher/',
-        '{"query" : "MATCH (n:Player) RETURN n LIMIT 25", "params" : {"id" : ".*' + $scope.searchText + '.*"} }')
-    // console.log('http://localhost:7474/db/data/cypher/',
-    //   '{"query" : "MATCH (p1:Player ' + '{ name:"' + name1 +
-    //              '" }),' + '(p2:Player { name:"' + name2 +
-    //              '" }), p = shortestPath((p1)-[*]-(p2)) RETURN' + ' p;" }');
-    // $http.post('http://localhost:7474/db/data/cypher/',
-    //   '{"query" : "MATCH (p1' + '{ name:"' + name1 +
-    //              '" }),' + '(p2{ name:"' + name2 +
-    //              '" }), p = shortestPath((p1)-[*]-(p2)) RETURN' + ' p;" ,"params": {} }')
-      .success(function(data) {
-        // $scope.dataset = data.data;
-        console.log("data",data);
-        console.log("data.data[0]",data.data[0]);
-        console.log("data.data[0][0]",data.data[0][0]);
-        console.log("data.data[0][0].data",data.data[0][0].data);
-      })
-      .error(function() {
-        console.log('Failed!');
-      });
+    var query = 'MATCH (p1:Player { name:"' +
+      $scope.searchText.name + '" })' + ',(p2:Player{ name:"' +
+      $scope.search.name + '" }),' +
+      ' p = shortestPath((p1)-[*]-(p2)) RETURN EXTRACT(n in nodes(p) | n.name), EXTRACT (n in nodes(p) | n.year)';
+    console.log('query', query);
+   $http({
+     method:"post",
+     url: "http://localhost:7474/db/data/cypher",
+     accepts: "application/json",
+     datatype:"json",
+     //contenttype:"application/json",
+     data:{ "query" : query },
+     success: function(){},
+     error:function(jqxhr, textstatus, errorthrown){ console.log('Failed!', textstatus); }
+   })//end of placelist ajax
+    .success(function(data) {
+      // $scope.dataset = data.data;
+      // console.log("data",data);
+      // console.log("data.data",data.data);
+      // console.log("data.data[0]",data.data[0]);
+      console.log("data.data[0][0]",data.data[0][0]);
+    })
   };
-
-//   $scope.callDB = function(name1, name2) {
-//               function (err, result) {
-//                 if (err) { console.log(err); }
-//                 if (result) { console.log('Query Working...', result) };
-//     //console.log(result.data); // delivers an array of query results
-//     //console.log(result.columns); // delivers an array of names of objects getting returned
-//                 }
-
-//               );
-//       // GET request to build CYPHER query
-//       $http({
-//         url: '/graph',
-//         method: "GET",
-//         data: [],
-//         params: { name1: $scope.searchText.name.toLowerCase(),
-//                   name2: $scope.search.name.toLowerCase() }
-//       }).success(function(data, status) {
-//           $scope.response = "GET Response: " + JSON.stringify(data.args);
-//           $scope.fullResponse = JSON.stringify(data);
-
-// db.cypherQuery( 'MATCH (p1:Player { name:"' + name1 +
-//                 '" }),(p2:Player { name:"' + name2 +
-//                 '" }), p = shortestPath((p1)-[*]-(p2)) RETURN p;',
-//               function (err, result) {
-//                 if (err) { console.log(err); }
-//                 if (result) { console.log('Query Working...', result) };
-//     //console.log(result.data); // delivers an array of query results
-//     //console.log(result.columns); // delivers an array of names of objects getting returned
-//                 }
-
-//               );
-//       });
-//   };
- });
+});
