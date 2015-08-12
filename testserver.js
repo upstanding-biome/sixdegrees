d57513d264f7689e34f0471dd0a5aea759750d85
Cristian Avalos <cavalos0086@gmail.com>
2015-08-12 12:55:40 -0700

    before parsing inputs

--- testserver.js
+++ testserver.js
@@ -6,27 +6,46 @@ db = new neo4j('http://localhost:7474/');
 
 //Run raw cypher with params
 db.cypherQuery(
-  'LOAD CSV FROM "file:///Users/CristianAvalos/Desktop/Telegraph_projects/Week6/Greenfield_project/sixdegrees/MasterDB.csv" AS line MERGE(p:Player {name:line[0]}) MERGE(t:Team {name:line[2], year:line[1]})', function (err, result) {
+  'LOAD CSV FROM "file:///Users/CristianAvalos/Desktop/Telegraph_projects/Week6/Greenfield_project/sixdegrees/TEST2.csv" AS line MERGE(p:Player {name:line[0]}) MERGE(t:Team {name:line[2], year:line[1]})', function (err, result) {
     if (err) {
       console.log(err);
     }
     if (result) {
       db.cypherQuery(
-       'LOAD CSV FROM "file:///Users/CristianAvalos/Desktop/Telegraph_projects/Week6/Greenfield_project/sixdegrees/MasterDB.csv" AS line MATC functionH (p:Player), (t:Team) WHERE p.name = line[0] AND t.name = line[2] AND t.year = line[1] CREATE (p)-[r:PLAYS_IN]->(t)', function(err,result){
+        'LOAD CSV FROM "file:///Users/CristianAvalos/Desktop/Telegraph_projects/Week6/Greenfield_project/sixdegrees/TEST2.csv" AS line MATCH (p:Player), (t:Team) WHERE p.name = line[0] AND t.name = line[2] AND t.year = line[1] CREATE (p)-[r:PLAYS_IN]->(t)', function(err,result){
         if(err){
           console.log('there was an error running the query', err);
         } else if(result){
-          console.log('Successfully Created Player and Team connections')
+          console.log('Successfully Created Player and Team connections');
+// EXTRACT(n in nodes(p) | n.name)'
+          db.cypherQuery(
+            'MATCH (p1:Player {name:"Russ Smith"}), (p2:Player {name:"Alex Len"}), p = shortestPath( (p1) - [*]-(p2)  ) RETURN EXTRACT(n in nodes(p) | n.name), EXTRACT(n in nodes(p) | n.year) ', function(err, p){
+              if(err){
+                console.log('error', err);
+              } else{
+                var data = p.data[0];
+                
+                console.log(p.data[0]);
+                
+                console.log('success');
+              }
+            });
         }
       });
-      console.log('Successfully Create Player and Team nodes')
+      console.log('Successfully Create Player and Team nodes');
     }
-    //console.log(result.data); // delivers an array of query results
-    //console.log(result.columns); // delivers an array of names of objects getting returned
   }
-);
+  );
+
+
+// MATCH (p1:Player { name:"Russ Smith" }),(p2:Player { name:"Alex Len" }),
+//   p = shortestPath((p1)-[*]-(p2))
+// RETURN p
 
 
 
 
 
+// MATCH (p1:Player { name:"Russ Smith" }),(p2:Player { name:"Alex Len" }),
+//   p = shortestPath((p1)-[*]-(p2))
+//   RETURN p
