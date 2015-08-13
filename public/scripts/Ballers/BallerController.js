d0ee73cdba00b9c7040c91196faf7bfef7b26720
ChristopherSalam <christopher.salam@gmail.com>
2015-08-12 17:47:11 -0700

    This version has correct input processing and sends a completed relationshop statement with good inputs. Pull this version down and restart your database.

--- public/scripts/Ballers/BallerController.js
+++ public/scripts/Ballers/BallerController.js
@@ -9,26 +9,50 @@ app.controller('BallerController', function($scope, $http){
 
   $scope.callDB = function(name1, name2) {
     var query = 'MATCH (p1:Player { name:"' +
-      $scope.searchText.name + '" })' + ',(p2:Player{ name:"' +
-      $scope.search.name + '" }),' +
-      ' p = shortestPath((p1)-[*]-(p2)) RETURN EXTRACT(n in nodes(p) | n.name), EXTRACT (n in nodes(p) | n.year)';
-    console.log('query', query);
+      $scope.searchText.name.toLowerCase() + '" })' + ',(p2:Player{ name:"' +
+      $scope.search.name.toLowerCase() + '" }),' +
+      ' p = shortestPath((p1)-[*]-(p2)) RETURN EXTRACT(n in nodes(p) | n.name), EXTRACT(n in nodes(p) | n.year), RELATIONSHIPS(p)';
    $http({
      method:"post",
      url: "http://localhost:7474/db/data/cypher",
      accepts: "application/json",
      datatype:"json",
-     //contenttype:"application/json",
      data:{ "query" : query },
      success: function(){},
      error:function(jqxhr, textstatus, errorthrown){ console.log('Failed!', textstatus); }
    })//end of placelist ajax
     .success(function(data) {
-      // $scope.dataset = data.data;
-      // console.log("data",data);
-      // console.log("data.data",data.data);
-      // console.log("data.data[0]",data.data[0]);
-      console.log("data.data[0][0]",data.data[0][0]);
+$scope.dataset = '';
+
+   var players = [];
+   var teams = [];
+   var years = [];
+
+   for( var i = 0; i < data.data[0][0].length; i++){
+     if(i%2 === 0){
+       players.push(data.data[0][0][i]);
+     } else{
+       teams.push(data.data[0][0][i]);
+     }
+   }
+
+   for(var i =0 ; i < data.data[0][1].length; i++){
+     if(data.data[0][1][i] !== null){
+       years.push(data.data[0][1][i]);
+     }
+   }
+
+    var str = '';
+
+    for( var i = 0; i < teams.length; i++){
+     str += players[i] + ' played in ' + teams[i] + ' in ' + years[i] +' with ';
+     if(i === teams.length- 1){
+       str += players[i+1];
+     }
+    }
+
+
+    $scope.dataset = str;
     })
   };
 });
