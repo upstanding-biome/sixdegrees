d70701f9fba2bb0ea9e0a400ec2e847c2e2e5eeb
Christopher Salam <christopher.salam@gmail.com>
2015-08-13 06:34:48 -0700

    Config changes.

--- public/app.js
+++ public/app.js
@@ -4,8 +4,11 @@ app.config(['$stateProvider', '$urlRouterProvider', '$httpProvider' , function($
   
   // delete header from client:
   // http://stackoverflow.com/questions/17289195/angularjs-post-data-to-external-rest-api
-  // $httpProvider.defaults.useXDomain = true;
-  // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.withCredentials = true;
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
+  $httpProvider.defaults.headers.common["Accept"] = "application/json";
+  $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 
   $stateProvider
     // route to show our basic form (/form)
